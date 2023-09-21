ebe3fad79a23589fb0cc91ff090124f813b37816
Eric Smith <essmith20@students.desu.edu>
2023-09-21 17:36:52 -0400

    NewProject3

---
+++ app/src/main/java/com/example/newproject3/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.newproject3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newproject3.ui.theme.NewProject3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+
+            }
+        }
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Log in to continue.",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewProject3Theme {
+        Greeting("Android")
+    }
+}
